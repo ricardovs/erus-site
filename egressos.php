5072d209630e1309e7fc94476a0079aee504cb3f
Ricardo Vieira <ric1500ric@gmail.com>
2018-05-02 14:44:35 -0300

    Alterando fundos do menu, e do slider. Editando o slider

--- egressos.php
+++ egressos.php
@@ -1,5 +1,12 @@
 <?php require("header.php") ?>
 <section id="egressos">
-    <h1>Ex-Membros</h1>
+    <h1>Ex-Membros</h1
+      <p>
+      Roberta Lima Gomes
+      Raquel Frizera Vassallo
+      Jadir Eduardo Souza Lucas
+      Patrícia Dockhorn Costa - Coordenadora atual
+    </p>
+
 </section>
 <?php require("footer.php") ?>
--- professores.php
+++ professores.php
@@ -1,5 +1,24 @@
 <?php require("header.php") ?>
 <section id="professores">
     <h1>Professores</h1>
+    <section class="slider-container">
+      <div class="center slider rounded">
+
+        <div>
+          <img class="slider-img" src="">
+        <p class="slider-header">Caio Borgo</p>
+        <p class="slider-text"> Engenharia Elétrica, 4. Simulação Computacional e Inteligência Artificial.</p>
+         </div>
+
+  		  <div>
+            <img class="slider-img" src="">
+            <p class="slider-header">Carolina  Martins Cunha</p>
+            <p class="slider-text"> Engenharia Mecânica, 5. Robótica, Biotecnologia, e Programação.</p>
+          </div>
+
+
+          </div>
+
+      </section>
 </section>
 <?php require("footer.php") ?>
